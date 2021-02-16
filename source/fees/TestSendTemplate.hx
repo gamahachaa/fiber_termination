81e5c01d241e94e855f380c7a2492a1e733681d8
bbaudry <bruno.baudry@salt.ch>
2021-02-16 12:53:14 +0100

    correction door to door

---
+++ source/fees/TestSendTemplate.hx
@@ -0,0 +1,19 @@
+package fees;
+
+import tstool.process.TripletTemplate;
+import tstool.salt.SOTemplate;
+
+/**
+ * ...
+ * @author bb
+ */
+class TestSendTemplate extends TripletTemplate 
+{
+
+	public function new(soTemplate:SOTemplate) 
+	{
+		super(soTemplate);
+		
+	}
+	
+}
