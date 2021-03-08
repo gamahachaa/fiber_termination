1ff2acb42cb1ee2ebbdc11065c543dea688d7923
bbaudry <bruno.baudry@salt.ch>
2021-03-08 17:43:02 +0100

    adding off hours process

---
+++ source/tickets/_CreateTicketSixForOne.hx
@@ -0,0 +1,19 @@
+package tickets;
+
+import tstool.process.ActionTicket;
+import tstool.salt.SOTickets;
+
+/**
+ * ...
+ * @author bb
+ */
+class _CreateTicketSixForOne extends ActionTicket 
+{
+
+	public function new(ticket:SOTickets, ?resolved:Bool=false) 
+	{
+		super(ticket, resolved);
+		
+	}
+	
+}
